2fec9cadec01726bceb838e8f6f622237e02dc3c
Vincent BOLLOT <vincent.bollot@gmail.com>
2023-09-07 12:17:33 +0200

    git pull force

---
+++ force-git-pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Récupérer toutes les modifications des branches distantes
+git fetch --all
+
+# Réinitialiser votre branche actuelle pour correspondre à la branche distante spécifiée (changez branch-name)
+git reset --hard origin/branch-name
+
+# Afficher un message de confirmation
+echo "Git pull forcé effectué avec succès."
