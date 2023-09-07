eb2235b3fcdd8d4b495ca9b2f48359434bc3a2d3
Vincent BOLLOT <vincent.bollot@gmail.com>
2023-09-07 12:45:12 +0200

    ajout clean repo distant avant de pull force

--- force-git-pull.sh
+++ force-git-pull.sh
@@ -6,6 +6,9 @@
 # Récupérer toutes les modifications des branches distantes
 git fetch --all
 
+# Nettoyer le répertoire (supprimer les fichiers non suivis et les fichiers ignorés)
+git clean -fd
+
 # Réinitialiser votre branche actuelle pour correspondre à la branche distante spécifiée (changez branch-name)
 git reset --hard origin
 
