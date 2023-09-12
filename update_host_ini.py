3c19ff95a1ea254b899049730d74e08e78b928be
Pallas <vincent.bollot@gmail.com>
2023-09-12 11:11:30 +0200

    ok

--- update_host_ini.py
+++ update_host_ini.py
@@ -9,11 +9,22 @@ session = boto3.Session(region_name=region)
 # Créez un client EC2.
 ec2_client = session.client('ec2')
 
+# Lire le fichier hosts.ini et extraire les noms d'instances
+instances_a_recuperer = []
+
+with open('hosts.ini', 'r') as f:
+    for line in f:
+        if line.strip() and not line.startswith('#'):
+            parts = line.split()
+            if len(parts) > 0:
+                instance_name = parts[0]
+                instances_a_recuperer.append(instance_name)
+
 # Dictionnaire pour stocker les adresses IP publiques
 adresses_ip_publiques = {}
 
-# Récupérez les adresses IP publiques de toutes les instances EC2
-response = ec2_client.describe_instances()
+# Récupérez les adresses IP publiques des instances EC2 spécifiées
+response = ec2_client.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': instances_a_recuperer}])
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
         instance_name = None
@@ -24,9 +35,24 @@ for reservation in response['Reservations']:
         if instance_name:
             adresses_ip_publiques[instance_name] = instance['PublicIpAddress']
 
-# Générer le contenu du fichier hosts.ini à partir du dictionnaire
-with open('hosts.ini', 'w') as f:
-    for instance_name, ip in adresses_ip_publiques.items():
-        f.write(f"{instance_name} ansible_host={ip} ansible_ssh_private_key_file=/home/ec2-user/ansible_vincent.pem ansible_user=ec2-user\n")
+# Mettre à jour le fichier hosts.ini avec les nouvelles adresses IP publiques
+with open('hosts.ini', 'r') as f:
+    lines = f.readlines()
 
-print("Le fichier hosts.ini a été généré avec les nouvelles adresses IP publiques.")
+with open('hosts.ini', 'w') as f:
+    for line in lines:
+        if line[0].strip() == "[":
+            f.write("\n"+line)
+        if line.strip() and not line.startswith('#'):
+            parts = line.split()
+            if len(parts) > 0:
+                instance_name = parts[0]
+                if instance_name in adresses_ip_publiques:
+                    new_ip = adresses_ip_publiques[instance_name]
+                    f.write(f"{instance_name} ansible_host={new_ip} ansible_ssh_private_key_file=/home/ec2-user/ansible_vincent.pem ansible_user=ec2-user\n")
+                else:
+                    f.write(line)
+        else:
+            f.write(line)
+
+print("Le fichier hosts_updated.ini a été créé avec les nouvelles adresses IP publiques.")
